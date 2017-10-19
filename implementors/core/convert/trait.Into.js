ba4f1e430ec4faded70225c701ec078ecb259108
Travis CI User <travis@example.org>
2017-10-19 14:53:13 +0000

    Documentation for panicbit/fcm-rust@9b8bf60

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["hyper"] = ["impl&lt;B&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"enum\" href=\"tokio_proto/streaming/message/enum.Message.html\" title=\"enum tokio_proto::streaming::message::Message\">Message</a>&lt;__ProtoResponse, B&gt;&gt; for <a class=\"struct\" href=\"hyper/struct.Response.html\" title=\"struct hyper::Response\">Response</a>&lt;B&gt;",];
+implementors["unicase"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;&amp;'a <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"unicase/struct.UniCase.html\" title=\"struct unicase::UniCase\">UniCase</a>&lt;&amp;'a <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt;","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"struct\" href=\"unicase/struct.UniCase.html\" title=\"struct unicase::UniCase\">UniCase</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/fcm/trait.Service.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fcm"] = ["impl <a class=\"trait\" href=\"fcm/trait.Service.html\" title=\"trait fcm::Service\">Service</a> for <a class=\"struct\" href=\"fcm/struct.Client.html\" title=\"struct fcm::Client\">Client</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
